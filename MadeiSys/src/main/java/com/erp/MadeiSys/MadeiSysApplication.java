1ba69f5dc7f956d72fee8e3f346aacbe7927b73e
João <joao.antonio2452@outlook.com>
2025-07-31 09:13:43 -0300

    feat: Firsth Commit

---
+++ MadeiSys/src/main/java/com/erp/MadeiSys/MadeiSysApplication.java
@@ -0,0 +1,13 @@
+package com.erp.MadeiSys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MadeiSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MadeiSysApplication.class, args);
+	}
+
+}
---
+++ MadeiSys/src/test/java/com/erp/MadeiSys/MadeiSysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erp.MadeiSys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MadeiSysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
